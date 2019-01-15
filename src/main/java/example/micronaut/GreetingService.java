a1a333bb0b0ec30ac4cde0d2e70cdb8d30e81e72
Iván López <lopez.ivan@gmail.com>
2019-01-15 15:45:55 +0100

    Add POJO, Controller and Service

---
+++ src/main/java/example/micronaut/GreetingService.java
@@ -0,0 +1,13 @@
+package example.micronaut;
+
+import io.reactivex.Single;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class GreetingService {
+
+    public Single<Greeting> sayHi(String name) {
+        return Single.just(new Greeting("Hello " + name));
+    }
+}

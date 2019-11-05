53bc9f9688660ac1f591ac663df8c17675bd152e
Iván López <lopezi@objectcomputing.com>
2019-11-05 15:52:21 +0100

    Add --static to link libc

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/basic-app-*-all.jar
+native-image --no-server --static --no-fallback --class-path build/libs/basic-app-*-all.jar

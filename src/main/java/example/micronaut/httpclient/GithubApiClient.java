fb8413cb67946cab39a92d023a71ca18d68f91c6
Iván López <lopezi@objectcomputing.com>
2021-05-05 13:08:52 +0200

    Use GitHub API instead of Bintray for testing HTTP Client

---
+++ src/main/java/example/micronaut/httpclient/GithubApiClient.java
@@ -0,0 +1,21 @@
+package example.micronaut.httpclient;
+
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
+import io.micronaut.http.client.annotation.Client;
+import io.reactivex.Flowable;
+
+import static io.micronaut.http.HttpHeaders.ACCEPT;
+import static io.micronaut.http.HttpHeaders.USER_AGENT;
+
+@Client(GithubApiClient.GITHUB_API_URL)
+@Header(name = USER_AGENT, value = "Micronaut HTTP Client")
+@Header(name = ACCEPT, value = "application/vnd.github.v3+json, application/json")
+public interface GithubApiClient {
+
+    String GITHUB_API_URL = "https://api.github.com";
+    String GITHUB_RELEASES_URI = "/repos/micronaut-projects/micronaut-core/releases";
+
+    @Get(GITHUB_RELEASES_URI)
+    Flowable<GithubRelease> fetchReleases();
+}
